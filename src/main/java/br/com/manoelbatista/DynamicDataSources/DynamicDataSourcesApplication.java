7b05ecff19b155a791c3fe96fe048de12ea84b04
Manoel Batista <manoelbatista902@gmail.com>
2024-02-23 12:51:32 -0300

    initial commit

---
+++ src/main/java/br/com/manoelbatista/DynamicDataSources/DynamicDataSourcesApplication.java
@@ -0,0 +1,13 @@
+package br.com.manoelbatista.DynamicDataSources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicDataSourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicDataSourcesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/manoelbatista/DynamicDataSources/DynamicDataSourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.manoelbatista.DynamicDataSources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicDataSourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
