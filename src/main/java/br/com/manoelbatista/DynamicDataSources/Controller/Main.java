c5ae3b51357d0429536991c3c25befe75705482d
Manoel Batista <manoelbatista902@gmail.com>
2024-02-25 14:29:25 -0300

    initial commit complement

---
+++ src/main/java/br/com/manoelbatista/DynamicDataSources/Controller/Main.java
@@ -0,0 +1,17 @@
+
+package br.com.manoelbatista.DynamicDataSources.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Manoel Batista <manoelbatista902@gmail.com>
+ * Main controller to test 
+ */
+@RestController
+@RequestMapping("/")
+@CrossOrigin
+public class Main {    
+    
+}
